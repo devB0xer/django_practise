0eedda97ba2dca63c765cccb9c7ea4e02d351e4b
JeANSdv <bazuev.4@yandex.ru>
2024-09-12 22:54:08 +0400

    2nd is dome

--- pra2/library/views.py
+++ pra2/library/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 def index(request):
     return render(request, 
-                  'index.html', 
+                  'index1.html', 
                   {
-                      'book': 'some... book'
+                      'book_link': 'https://www.litres.ru/book/aleksandr-pushkin/kapitanskaya-dochka-171967/',
+                      'book_img': 'https://cdn.litres.ru/pub/c/cover_415/171967.webp',
                   })
