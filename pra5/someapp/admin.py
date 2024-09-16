7d32af59a5c3ca9a0dbecc5f7a7444c94903a0c7
devB0xer <bazuev.4@yandex.ru>
2024-09-16 10:39:22 +0400

    practice 5 is done

--- pra5/someapp/admin.py
+++ pra5/someapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from someapp.models import Author, Book, Publisher
 
-# Register your models here.
+@admin.register(Author)
+class AdminAuthor(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class AdminBook(admin.ModelAdmin):
+    pass
+
+@admin.register(Publisher)
+class AdminPublisher(admin.ModelAdmin):
+    pass
