3ad83ba07c57efabb1f626b758d79c0ddbc6f34f
devB0xer <bazuev.4@yandex.ru>
2024-09-19 11:04:54 +0400

    6th is done

--- pra6_pra5-copy/someapp/views.py
+++ pra6_pra5-copy/someapp/views.py
@@ -3,9 +3,6 @@ from .models import Book
 
 # Create your views here.
 
-def somePage(request):
-    return HttpResponse('<h1>hi</h1>')
-
 def getInfo(request):
     book = Book.objects.get(id = 1)
     return render(request, 'base.html', {
@@ -13,4 +10,19 @@ def getInfo(request):
     })
 
 def getAuthor(request):
-    bookAuthor = Book.objects.filter(author__name = "") #unfinished here!
+    bookAuthor = Book.objects.filter(author__name = "Pushkin")
+    return render(request, 'author.html', {
+        'bookAuthor': bookAuthor,
+    })
+
+def getAuthorNoPushkin_lmao(request):
+    bookAuthor = Book.objects.filter(author__name = "someAuthor")
+    return render(request, 'author.html', {
+        'bookAuthor': bookAuthor,
+    })
+
+def getPublisher(request):
+    bookPublisher = Book.objects.filter(publishers__name = "somePublisher")
+    return render(request, 'publishers.html', {
+        'bookPublisher': bookPublisher,
+    })
