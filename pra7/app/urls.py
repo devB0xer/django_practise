d483ef6746e7f7cdd3ce390f7b07545ff9d47bd8
devB0xer <bazuev.4@yandex.ru>
2024-09-24 18:42:41 +0400

    pra7 in progress

--- pra7/app/urls.py
+++ pra7/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,showProducts
 
 urlpatterns = [
     path('', index),
+    path('products/', showProducts),
 ]
--- pra7/app/views.py
+++ pra7/app/views.py
@@ -1,21 +1,13 @@
 from typing import Any
 from django.http import HttpRequest
-from django.http.response import HttpResponse as HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
-
-
-
-
-class ViewHome(TemplateView):
-    template_name = 'home.html'
-
-    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        return super().get(request, *args, **kwargs)
-    
-
-
 
 
 def index(request):
     return render(request, 'home.html')
+
+def showProducts(request):
+    return render(request, 'clothes/products.html', [
+        'clothes': '1' , 
+    ])
