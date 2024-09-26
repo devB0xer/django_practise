091a6ca3d119ed288636cbbf88a4533d051db5d2
devB0xer <bazuev.4@yandex.ru>
2024-09-26 12:37:15 +0400

    practice 7 is done

--- pra7/app/urls.py
+++ pra7/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index,showProducts
+from .views import index, showProducts, showAbout
 
 urlpatterns = [
     path('', index),
     path('products/', showProducts),
+    path('about/', showAbout),
 ]
--- pra7/app/views.py
+++ pra7/app/views.py
@@ -8,6 +8,15 @@ def index(request):
     return render(request, 'home.html')
 
 def showProducts(request):
-    return render(request, 'clothes/products.html', [
-        'clothes': '1' , 
-    ])
+    return render(request, 'clothes/products.html', {
+        'clothes': [ {'name': 'shirt', 
+                      'img': 'img/clothes/shirt.webp'}, 
+                      {'name': 'hat', 
+                      'img': 'img/clothes/hat.webp'},
+                      {'name': 'coat', 
+                      'img': 'img/clothes/coat.jpg'},
+                    ],
+    })
+
+def showAbout(request):
+    return render(request, 'about.html')
